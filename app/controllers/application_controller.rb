37fff15dce6ca3e31067962cbf78e815a1b0f00c
Nino Bessa <ninobessa@gmail.com>
2022-02-17 16:52:58 -0300

    feat(devise): added root route to app, pemit :name, :kind and :status params to devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_employee!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :kind, :status])
+  end
+
+
 end
