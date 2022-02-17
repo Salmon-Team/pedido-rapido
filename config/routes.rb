bf84de5b61c99ec6d2ed474ba87007bb0d5df522
Nino Bessa <ninobessa@gmail.com>
2022-02-17 14:57:42 -0300

    refactor devise route - login/logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-
+  devise_scope :employees do
+    get "/login" => "devise/sessions#new" # custom path to login/sign_in
+    get "/logout" => "devise/registrations#new", as: "new_user_registration" # custom path to sign_up/registration
+  end
   devise_for :employees
-  
-  # root to: "home#index"
 
+  # root to: "home#index"
   resources :products
 end
