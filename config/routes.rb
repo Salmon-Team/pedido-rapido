c58fc78bb7b26b98e0969a6d2ec3c8e3006fb6bb
Nino Bessa <ninobessa@gmail.com>
2022-02-17 15:07:18 -0300

    Fix custom path

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_scope :employees do
     get "/login" => "devise/sessions#new" # custom path to login/sign_in
-    get "/logout" => "devise/registrations#new", as: "new_employees_registration" # custom path to sign_up/registration
+    get "/logout" => "devise/registrations#new", as: "new_employee_registration" # custom path to sign_up/registration
   end
   devise_for :employees
 
