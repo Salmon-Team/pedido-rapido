356dd3bba7ff64f37f0e864f999681ce82004f83
Nino Bessa <ninobessa@gmail.com>
2022-02-18 20:42:20 -0300

    :sparkles: feat(employee): created CRUD to Employee

---
+++ app/controllers/employees_controller.rb
@@ -0,0 +1,50 @@
+class EmployeesController < ApplicationController
+  before_action :set_employee, only: %i[ show edit update]
+
+  def index
+    @employees = Employee.all
+  end
+
+  def show
+  end
+
+  def new
+    @employee = Employee.new
+  end
+
+  def create
+    @employee = Employee.new(employee_params)
+
+    respond_to do |format|
+      if @employee.save
+        format.html { redirect_to employee_url(@employee), notice: "Employee was successfully created." }
+      else
+        format.js
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    meth = params[:employee][:password].blank? ? "update_without_password" : "update"
+
+    respond_to do |format|
+      if @employee.send(meth, employee_params)
+        format.html { redirect_to employee_url(@employee), notice: "Employee was successfully updated." }
+      else
+        format.js
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end
+
+private
+    def set_employee
+      @employee = Employee.find(params[:id])
+    end
+
+    def employee_params
+      params.require(:employee).permit(:name, :kind, :status, :email, :password, :password_confirmation)
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  devise_scope :employees do
-    get "/login" => "devise/sessions#new" # custom path to login/sign_in
-    get "/logout" => "devise/registrations#new", as: "new_employee_registration" # custom path to sign_up/registration
-  end
-  devise_for :employees
-  
   root to: "products#index"
+
+  resources :employees, only: [:index, :show, :new, :create, :edit, :update]
+
+  devise_for :employees, :path => 'users', :path_names => { :sign_in => "login", :sign_out => "logout", :sign_up => "register" }
 
   resources :products
 
