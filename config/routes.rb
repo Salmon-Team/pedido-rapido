7cedb4537ec2e6e2cc7b510df0bc5f48d835f82e
Nino Bessa <ninobessa@gmail.com>
2022-02-18 22:24:26 -0300

    :sparkles: feat(employee): created CRUD to Category

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   devise_for :employees, :path => 'users', :path_names => { :sign_in => "login", :sign_out => "logout", :sign_up => "register" }
 
   resources :products
-
   resources :orders
+  resources :categories
 end
