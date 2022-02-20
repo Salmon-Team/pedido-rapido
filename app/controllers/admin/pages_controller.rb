04418ff5891b12c3dff69853f8777d0ba06f3c43
Nino Bessa <ninobessa@gmail.com>
2022-02-20 10:41:43 -0300

    :refactor: fix(pages controller): renamed Admin::StaticPagesController to Admin::PagesController and added Dashboard route

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -1,6 +1,9 @@
-class Admin::StaticPagesController < ApplicationController
+class Admin::PagesController < ApplicationController
   skip_before_action :authenticate_employee!, only: :home
   
   def home
+  end
+
+  def dashboard
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
       end
       resources :categories
       resources :orders
+      get '/dashboard', to: 'pages#dashboard'
     end
   end
 
